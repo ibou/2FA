4c155b2911267facae328d20d89f63b6b2d37c78
Ibrahima D <ibrahima.diallo@display-interactive.com>
2021-09-04 23:49:00 +0200

    2 factor auth

---
+++ src/Entity/User.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Scheb\TwoFactorBundle\Model\Email\TwoFactorInterface;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
+ */
+class User
+    implements UserInterface,
+               PasswordAuthenticatedUserInterface,
+               TwoFactorInterface
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private ?int $id;
+    
+    /**
+     * @ORM\Column(type="string", length=180, unique=true)
+     */
+    private string $email;
+    
+    /**
+     * @ORM\Column(type="json")
+     */
+    private array $roles = [];
+    
+    /**
+     * @ORM\Column(type="string")
+     */
+    private string $password;
+    
+    /**
+     * @ORM\Column(type="string", nullable=true)
+     */
+    private string $authCode;
+    
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+    
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+    
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+        
+        return $this;
+    }
+    
+ 
+    public function getUserIdentifier(): string
+    {
+        return (string)$this->email;
+    }
+    
+    /**
+     * @deprecated since Symfony 5.3, use getUserIdentifier instead
+     */
+    public function getUsername(): string
+    {
+        return (string)$this->email;
+    }
+    
+ 
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+        
+        return array_unique($roles);
+    }
+    
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+        
+        return $this;
+    }
+    
+ 
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+    
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+        
+        return $this;
+    }
+    
+ 
+    public function getSalt(): ?string
+    {
+        return null;
+    }
+    
+ 
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+    
+    
+    public function isEmailAuthEnabled(): bool
+    {
+        return true; // This can be a persisted field to switch email code authentication on/off
+    }
+    
+    public function getEmailAuthRecipient(): string
+    {
+        return $this->email;
+    }
+    
+    public function getEmailAuthCode(): string
+    {
+        if (null === $this->authCode) {
+            throw new \LogicException('The email authentication code was not set');
+        }
+        
+        return $this->authCode;
+    }
+    
+    public function setEmailAuthCode(string $authCode): void
+    {
+        $this->authCode = $authCode;
+    }
+}
